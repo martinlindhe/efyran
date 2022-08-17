44758c8891b0925f32a615af5190cb8e4988eb3d
Martin Lindhe <martin@ubique.se>
2022-08-17 11:02:48 +0200

    buffs: handle item clickies

--- e4_Buffs.lua
+++ e4_Buffs.lua
@@ -4,19 +4,40 @@ local Buffs = {}
 local settingsRoot = 'D:/dev-mq/mqnext-e4-lua/settings'
 
 function Buffs.RefreshBuffs()
-    print('RefreshBuffs XXX ', mq.TLO.Me.Class.ShortName, ' ', mq.TLO.Time)
+    print('-- RefreshBuffs ', mq.TLO.Me.Class.ShortName, ' ', mq.TLO.Time)
 
     local settingsFile = settingsRoot .. '/' .. mq.TLO.MacroQuest.Server() .. '_' .. mq.TLO.Me.Class.ShortName() .. '_' .. mq.TLO.Me.Name() .. '.ini'
 
     for n = 1,20,1
     do
-        local buff = mq.TLO.Ini(settingsFile, 'Buffs', 'Buff' .. n)()
-        if buff ~= nil then
-            print(type(buff), 'XXX cast ', buff)
+        local buffItem = mq.TLO.Ini(settingsFile, 'Buffs', 'Buff' .. n)()
+        if buffItem ~= nil then
 
-            -- XXX check my buffs. ..
+            local spell
+            if mq.TLO.FindItem(buffItem).ID() ~= nil then
+                spell = mq.TLO.FindItem(buffItem).Clicky.Spell
+                --print('using clicky', buffItem, spell)
+            elseif mq.TLO.Me.Book(buffItem)() ~= nil then
+                spell = mq.TLO.Me.Book(mq.TLO.Me.Book(buffItem))
+                --print('using book', buffItem, spell)
+            else
+                print('ERROR cant find buff', buffItem)
+            end
 
-            --  mq.TLO.Me.Buff('Flight of Eagles')
+            -- print("considering buffing ", spell)
+
+            if mq.TLO.Me.Buff(spell.Name()).ID() == nil then
+                print('refreshing buff ', spell.Name())
+
+                if spell.TargetType() == "Self" then
+                    mq.cmd.casting('"' .. buffItem .. '"')
+                else
+                    mq.cmd.casting('"' .. buffItem .. '" -targetid|'.. mq.TLO.Me.ID())
+                end
+
+                -- end loop after first successful buff
+                break
+            end
         end
     end
 
