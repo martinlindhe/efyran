a327186a730bc2618298f6fb0b43175dafee1138
Martin Lindhe <martin@ubique.se>
2022-08-17 11:02:03 +0200

    group: auto form raid (1st group leader becomes raid leader)

--- e4_Group.lua
+++ e4_Group.lua
@@ -1,9 +1,6 @@
--- TODO later: auto reform raid
 -- TODO later: register "/savegroup" command
 
 local Group = {}
-
-local onlyAllowBots = true
 
 -- FIXME: relative path...
 local settingsRoot = 'D:/dev-mq/mqnext-e4-lua/settings'
@@ -11,15 +8,18 @@ local settingsRoot = 'D:/dev-mq/mqnext-e4-lua/settings'
 function Group.Init()
 
     mq.bind('/recallgroup', function(name, groupNumber)
-        -- recalls group setup from INI, leader from group 1 will tell other leaders to form up
+        -- recalls group setup from INI, orchestrator (caller) will tell the rest how to form up
 
-        print("recallgroup ", name, " ", groupNumber)
-
+        local orchestrator = false
+        local raidLeader = ""
         if groupNumber == nil then
-            -- XXX separate command /dropgroup:
+            orchestrator = true
+
             mq.cmd.noparse('/dgaexecute /if (${Raid.Members} > 0) /raiddisband')
             mq.cmd.noparse('/dgaexecute /if (${Group.Members} > 0) /disband')
-            mq.delay(2000)
+            mq.delay(1000)
+        else
+            print('Recalling group ', name, ' ', groupNumber)
         end
 
         local server = mq.TLO.MacroQuest.Server()
@@ -27,9 +27,12 @@ function Group.Init()
         for g = 1,12,1
         do
             group = server .. '_' .. name .. '_' .. g
-            local leader = mq.TLO.Ini(settingsRoot .. '/Saved Groups.ini', group, 'Member1', 'NULL')()
+            local groupLeader = mq.TLO.Ini(settingsRoot .. '/Saved Groups.ini', group, 'Member1', 'NULL')()
+            if g == 1 then
+                raidLeader = groupLeader
+            end
 
-            if mq.TLO.Me.Name() == leader then
+            if mq.TLO.Me.Name() == groupLeader then
                 for n = 2,6,1
                 do
                     local member = "Member" .. n
@@ -44,51 +47,55 @@ function Group.Init()
                         end
                     end
                 end
-            elseif groupNumber == nil and leader ~= 'NULL' then
-                print('TELLLING LEADER ', leader, ' TO FORM GROUP ', group)
-                mq.cmd.dexecute(leader, '/recallgroup', name, g)
+            elseif orchestrator and groupLeader ~= 'NULL' then
+                print('Telling group leader ', groupLeader, ' to form group ', group)
+                mq.cmd.dexecute(groupLeader, '/recallgroup', name, g)
+            end
+        end
+
+        if orchestrator then
+            mq.cmd.dgtell('Recalling raid', name, 'with leader', raidLeader)
+            mq.delay(2000)
+
+            for g = 1,12,1
+            do
+                group = server .. '_' .. name .. '_' .. g
+                local groupLeader = mq.TLO.Ini(settingsRoot .. '/Saved Groups.ini', group, 'Member1', 'NULL')()
+    
+                if groupLeader ~= 'NULL' then
+                    if mq.TLO.Me.Name() == raidLeader then
+                        mq.cmd.raidinvite(groupLeader)
+                    elseif raidLeader ~= groupLeader then
+                        --print('Telling raid leader ', raidLeader,' to invite', groupLeader)
+                        mq.cmd.dexecute(raidLeader, '/raidinvite', groupLeader)
+                    end
+                    -- mq.delay(50)
+                end
             end
         end
     end)
 
     mq.event('joingroup', '#1# invites you to join a group.', function(text, sender)
-        -- Accepts group invites
-
-        -- only allow invites from my bots
-        if onlyAllowBots and mq.TLO.DanNet(sender) == nil then
+        -- Accepts group invites my bots
+        if mq.TLO.DanNet(sender) == nil then
             mq.cmd.dgtell('WARNING: IGNORING GROUP INVITE FROM NON-BOT ', sender)
             return
         end
-
-        mq.cmd.dgtell('GROUP INVITE FROM ' .. sender)
-
-        --[[
-        /if (${Me.Casting.ID} && ${Me.Class.ShortName.NotEqual[BRD]}) {
-            /call interrupt
-            /delay 5s !${Me.Casting.ID}
-          }
-        ]]--
-
+        -- mq.cmd.dgtell('GROUP INVITE FROM ' .. sender)
         mq.cmd.squelch('/target clear')
-        mq.delay(500)
+        mq.delay(200)
         mq.cmd.squelch('/invite')
+    end)
 
-        --[[
-          /declare retryTimer timer local 5s
-          :retry_Invite
-          /squelch /target clear
-          /delay 3
-          /invite
-          /delay 1s ${Bool[${Group}]}
-          /if (!${Bool[${Group}]}) {
-            /if (${retryTimer}) {
-              /goto :retry_Invite
-            } else {
-              /echo Failed to join the group.
-            }
-          }
-        ]]--
-
+    mq.event('joinraid', '#1# invites you to join a raid.#*#', function(text, sender)
+        -- Accepts raid invites from my bots
+        if mq.TLO.DanNet(sender) == nil then
+            mq.cmd.dgtell('WARNING: IGNORING RAID INVITE FROM NON-BOT ', sender)
+            return
+        end
+        -- mq.cmd.dgtell('RAID INVITE FROM ' .. sender)
+        mq.cmd.notify('ConfirmationDialogBox Yes_Button leftmouseup')
+        mq.cmd.squelch('/raidaccept')
     end)
 
     print('DONE: Group.Init')
