7e9ad79ef3585dea6adf48f662b732ab8bde4818
Aleksander Spro <abspro@gmail.com>
2023-11-02 17:01:15 +0100

    Add force flag for setting delete for no drop items

--- lib/looting/markItemForDestroying.lua
+++ lib/looting/markItemForDestroying.lua
@@ -2,7 +2,7 @@ local mq = require("mq")
 local log = require("knightlinc/Write")
 local repository = require("lib/looting/repository")
 
-local function markItemForDestroying()
+local function markItemForDestroying(force)
     local cursor = mq.TLO.Cursor
     if not cursor() then
         log.Error("No item to mark for destroying on cursor")
@@ -12,6 +12,12 @@ local function markItemForDestroying()
     local lootItem = repository:get(cursor)
     if lootItem ~= nil and lootItem.Destroy then
         log.Debug("Item %s already marked for destroying", cursor.ItemLink("CLICKABLE")())
+    end
+
+    if cursor.NoDrop() and force ~= "force" then
+        all_tellf("ERROR: cant mark NO-DROP item for destroy. use '/setdestroyitem force' to force marking this item for deleting")
+        cmd("/beep")
+        return
     end
 
     if lootItem ~= nil then
